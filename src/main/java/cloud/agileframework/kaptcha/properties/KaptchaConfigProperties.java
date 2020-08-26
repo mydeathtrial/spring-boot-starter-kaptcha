d7a43fa1a098203cda84db9274406c8c8fe41187
mydeathtrial@163.com <mydeathtrial@163.com>
2020-08-26 18:37:41 +0800

    feat(调整): 验证码组件增加默认配置参数

--- src/main/java/cloud/agileframework/kaptcha/properties/KaptchaConfigProperties.java
+++ src/main/java/cloud/agileframework/kaptcha/properties/KaptchaConfigProperties.java
@@ -1,5 +1,6 @@
 package cloud.agileframework.kaptcha.properties;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.time.Duration;
@@ -9,7 +10,7 @@ import java.util.Properties;
  * @author by 佟盟 on 2018/2/1
  */
 @ConfigurationProperties(prefix = "agile.kaptcha")
-public class KaptchaConfigProperties {
+public class KaptchaConfigProperties implements InitializingBean {
     /**
      * 开关
      */
@@ -29,12 +30,13 @@ public class KaptchaConfigProperties {
     /**
      * 过期时间
      */
-    private Duration liveTime;
+    private Duration liveTime = Duration.ofMinutes(1);
 
     /**
      * 样式属性
      */
     private Properties properties;
+
 
     public boolean isEnable() {
         return enable;
@@ -82,5 +84,25 @@ public class KaptchaConfigProperties {
 
     public void setProperties(Properties properties) {
         this.properties = properties;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (properties == null) {
+            properties = new Properties();
+            properties.put("kaptcha.background.clear.from", "45,45,45");
+            properties.put("kaptcha.background.clear.to", "45,45,45");
+            properties.put("kaptcha.noise.color", "white");
+            properties.put("kaptcha.border", "yes");
+            properties.put("kaptcha.border.color", "white");
+            properties.put("kaptcha.textproducer.font.color", "white");
+            properties.put("kaptcha.textproducer.font.size", "40");
+            properties.put("kaptcha.image.width", "125");
+            properties.put("kaptcha.image.height", "45");
+            properties.put("kaptcha.textproducer.char.length", "4");
+            properties.put("kaptcha.textproducer.font.names", "微软雅黑");
+            properties.put("kaptcha.textproducer.impl", "cloud.agileframework.kaptcha.kaptcha.AgileTextProducer");
+            properties.put("kaptcha.obscurificator.impl", "com.google.code.kaptcha.impl.ShadowGimpy");
+        }
     }
 }
